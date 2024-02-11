be4d49a36bed5b8fa95bf7ea380c9df3b5fe3c64
marwan <abulibdeh43@gmail.com>
2024-02-11 17:54:52 -0600

    some code

---
+++ Lecture6.asm
@@ -0,0 +1,41 @@
+.global LSL_32
+.global LSR_32
+.global ASL_32
+.global ASR_32
+.global AND_32
+.global OR_3
+.global is_Even
+.text
+
+;This shifts (unit32_t x, unit32_t p), x << p
+LSL_32:
+    MOV R0, R0 LSL R1
+    bx lr
+;same thing but to the right
+LSR_32:
+    MOV R0, R0 LSR R1
+    bx lr
+;same thing as LSL
+ASL_32:
+    MOV R0, R0 LSL R1
+    bx lr
+;arithmetic to the right
+ASR_32:
+    MOV R0, R0 ASR R1
+    bx lr
+;logical AND{S}
+AND_32:
+    AND R0, R0, R1
+    bx lr
+;logical ORR{S}
+OR_32:
+    ORR R0, R0, R1
+    
+    bx lr
+;returns if the number is even or odd.
+is_Even:
+    TST R0, 1
+    MOVEQ R0, #0
+    MOVNE R0, #1
+
+    bx lr
